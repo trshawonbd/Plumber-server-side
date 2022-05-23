0bdc718028a843c44ada06ab71e4a272d9fac5dd
Your Name <you@example.com>
2022-05-23 04:40:29 +0300

    booked tool post  method done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -19,6 +19,7 @@ async function run() {
       console.log('db connected');
 
       const toolsCollection = client.db('plumber').collection('tools');
+      const bookedCollection = client.db('plumber').collection('booked');
 
 
       //Add Tools
@@ -38,6 +39,29 @@ async function run() {
   
         res.send(tools);
       })
+
+      //Get a single Tool
+
+      app.get('/tool/:id', async(req, res) =>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+          const details = await toolsCollection.findOne(query);
+
+          res.send(details);
+      })
+
+
+      // Add bookings
+
+      app.post('/booked', async(req, res) =>{
+          const booked = req.body;
+          console.log(booked);
+          const result = await bookedCollection.insertOne(booked);
+          return res.send({
+            success: true, result
+          });
+      })
+
 
 
     } finally {
