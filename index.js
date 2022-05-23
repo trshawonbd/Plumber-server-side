faf9fe8bdc19b217b148d2782249a9de53d220a8
Your Name <you@example.com>
2022-05-23 19:50:08 +0300

    admin  done

--- index.js
+++ index.js
@@ -156,6 +156,36 @@ async function run() {
         })
 
 
+        // get all users
+        app.get('/user',  async (req, res) => {
+
+            const users = await userCollection.find().toArray();
+            res.send(users);
+          })
+
+
+          //make admin
+          app.put('/user/admin/:email',  async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+              $set: { role: 'admin' },
+            };
+            const result = await userCollection.updateOne(filter, updateDoc);
+            console.log(result)
+            res.send(result);
+          })
+
+          // find admin
+
+          app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+          });
+
+
 
 
 
